43b8cba6f6c7968420c371add291fd6eb57ce98a
Maksim Vasic <vasicmaksim4@gmail.com>
2024-05-03 17:49:39 +0200

    Changes

--- Menu.py
+++ Menu.py
@@ -3,6 +3,7 @@ import os.path
 import pygame
 from Constants import *
 import Game
+
 #initialization of the game
 pygame.init()
 screen = pygame.display.set_mode(size=(WIDTH, HEIGHT))
@@ -35,30 +36,68 @@ class Button:
 
         return action
 
-forcejump = Button(pygame.font.Font("Jersey_25"+os.path.sep+"Jersey25-Regular.ttf", size=75).render("YES", True, (0, 0, 0)), 300, 500)
-regular = Button(pygame.font.Font("Jersey_25"+os.path.sep+"Jersey25-Regular.ttf", size=75).render("NO", True, (0, 0, 0)), 500, 500)
-start = Button(pygame.font.Font("Jersey_25"+os.path.sep+"Jersey25-Regular.ttf", size=100).render("START", True, (0, 0, 0)), 400, 650)
+
+
+
+#class main menu
+class Menu:
+    forcejump = Button(
+        pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("YES", True,
+                                                                                             (0, 0, 0)), 300, 500)
+    regular = Button(
+        pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("NO", True, (0, 0, 0)),
+        500, 500)
+    start = Button(
+        pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=100).render("START", True,
+                                                                                              (0, 0, 0)), 400, 650)
+    quit = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=50).render("EXIT", True,
+                                                                                                       (255, 0, 0)),
+                  400, 700)
+    mode = None
+    screen.fill((2, 122, 4))
+    screen.blit(welcome, welcome.get_rect(center=screen.get_rect().center, top=150))
+    screen.blit(gamemode, gamemode.get_rect(center=screen.get_rect().center, top=400))
+    if forcejump.draw(screen):
+        mode = True
+        forcejump = Button(
+            pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("YES", True, (255, 0, 0)),
+            300, 500)
+        regular = Button(
+            pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("NO", True, (0, 0, 0)),
+            500, 500)
+    if regular.draw(screen):
+        mode = False
+        forcejump = Button(
+            pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("YES", True, (0, 0, 0)),
+            300, 500)
+        regular = Button(
+            pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("NO", True, (255, 0, 0)),
+            500, 500)
+    if start.draw(screen):
+        game = Game.Game(mode)
+        start = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=100).render("START", True,(255, 0, 0)),400, 650)
+    if quit.draw(screen):
+        pygame.quit()
+        exit()
+class Board:
+    def __init__(self):
+        board = pygame.Surface((WIDTH, HEIGHT))
+        board.fill((181, 136, 99))
+        for x in range(0, 8, 1):
+            if x % 2 == 0:
+                for y in range(0, 8, 2):
+                    pygame.draw.rect(board, (240, 217, 181), (x * CELL, y * CELL, CELL, CELL))
+            else:
+                for y in range(1, 8, 2):
+                    pygame.draw.rect(board, (240, 217, 181), (x * CELL, y * CELL, CELL, CELL))
+        screen.blit(board, (0, 0))
 
 #game loop
 while True:
-    mode = None
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.fill((2, 122, 4))
-    screen.blit(welcome,  welcome.get_rect(center = screen.get_rect().center, top = 150))
-    screen.blit(gamemode,  gamemode.get_rect(center = screen.get_rect().center, top = 400))
-    if forcejump.draw(screen):
-        mode = True
-        forcejump = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("YES", True,(255, 0, 0)), 300, 500)
-        regular = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("NO", True,(0, 0, 0)), 500, 500)
-    if regular.draw(screen):
-        mode = False
-        forcejump = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("YES", True,(0, 0, 0)), 300, 500)
-        regular = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=75).render("NO", True, (255, 0, 0)),500, 500)
-    if start.draw(screen):
-        game = Game.Game(mode)
-        start = Button(pygame.font.Font("Jersey_25" + os.path.sep + "Jersey25-Regular.ttf", size=100).render("START", True,(255, 0, 0)), 400, 650)
+        Menu()
     pygame.display.update()
     clock.tick(60)
